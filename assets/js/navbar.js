d8e14acc465db6a9fbdf33e1eacf1d6add887098
franzi <cod3licious@gmail.com>
2021-11-04 22:59:27 +0100

    update consulting description

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,9 +1,17 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
+function showNavIcon() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
     x.className += " responsive";
   } else {
     x.className = "topnav";
   }
+}
+
+/* make the navbar tab of the current site active so users know where they are */
+function highlightTab() {
+  var url_name = window.location.href.toString().split("/").pop();
+  var id_name = "nav_" + url_name.substring(0, url_name.indexOf("."));
+  var x = document.getElementById(id_name);
+  x.classList.add("active");
 }
